6638aefbce5f7887ed1e7111811c0934738bf3e7
Nick Alteen <ncalteen@github.com>
2023-10-17 23:54:56 -0400

    Remove iframe from live preview wrapper

--- gatsby-theme-doctocat-typescript/src/components/live-preview-wrapper.tsx
+++ gatsby-theme-doctocat-typescript/src/components/live-preview-wrapper.tsx
@@ -1,8 +1,6 @@
-import { BaseStyles, Box, themeGet } from '@primer/react'
+import { Box, themeGet } from '@primer/react'
 
 import React, { ReactElement, ReactNode } from 'react'
-
-import Frame from './frame'
 
 // Users can shadow this file to wrap live previews.
 // This is useful for applying global styles.
@@ -12,16 +10,13 @@ export default function LivePreviewWrapper({
   children: ReactNode
 }): ReactElement {
   return (
-    <Frame>
-      <BaseStyles>
-        <Box
-          style={{
-            padding: themeGet('space.3')({}),
-            fontFamily: themeGet('fonts.normal')({})
-          }}>
-          {children}
-        </Box>
-      </BaseStyles>
-    </Frame>
+    <Box
+      style={{
+        padding: themeGet('space.3')({}),
+        fontFamily: themeGet('fonts.normal')({}),
+        width: '100%'
+      }}>
+      {children}
+    </Box>
   )
 }
