3ea83c94559a1d31a2c31a0c99775f3b1e7c277e
Nick Alteen <ncalteen@github.com>
2023-10-17 23:54:42 -0400

    Fix image theming

--- gatsby-theme-doctocat-typescript/src/components/image.tsx
+++ gatsby-theme-doctocat-typescript/src/components/image.tsx
@@ -1,10 +1,21 @@
-import { themeGet } from '@primer/react'
+import { themeGet, useTheme } from '@primer/react'
+
+import React, { ReactElement } from 'react'
 import { styled } from 'styled-components'
 
-const Image = styled.img`
+const StyledImage = styled.img`
   max-width: 100%;
   box-sizing: content-box;
-  background-color: ${themeGet('colors.white')};
+  background-color: ${(props) =>
+    themeGet(`colorSchemes.${props.theme.colorScheme}.colors.canvas.default`)(
+      props
+    )};
 `
+
+const Image = (props: any): ReactElement => {
+  const theme = useTheme()
+
+  return <StyledImage {...props} theme={theme} />
+}
 
 export default Image
